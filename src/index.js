2fd18d8287cf59ce9eb2c280c39c2a71cb353a6a
anasabusaaleek <anas.abusaaleek@gmail.com>
2023-06-12 23:46:02 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const books = [
+  {
+    title: 'Fourth Wing (The Empyrean, 1)',
+    author: 'Rebecca Yarros',
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/91qlE6JayPL._AC_UL600_SR600,400_.jpg',
+    id: 1,
+  },
+  {
+    title: 'Lessons in Chemistry: A Novel',
+    author: 'Bonnie Garmus',
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg',
+    id: 2,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 3,
+  },
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const BookList = () => {
+  
+  const gitBook =(id)=>{
+    const book=books.find((book)=>book.id===id)
+  console.log(book.title);
+  }
+
+  return (
+    <>
+      <h1>top books salles </h1>
+
+      <section className="booklist">
+        {books.map((book, index) => {
+          //destructuring
+
+          const { image, title, author, id } = book
+
+          // console.log(index)
+          return <Book {...book} gitBook={gitBook} index={index}></Book>
+        })}
+      </section>
+    </>
+  )
+}
+
+const Book = (props) => {
+  const { image, title, author,index /*children */, gitBook,id } = props
+  const gitSBook=()=>{
+gitBook(id)
+  }
+  return (
+    <article className="book">
+      <img src={image} />
+      <h2>{title}</h2>
+      <button onClick={gitSBook} >click me</button>
+      <h4> {author}</h4>
+      <span className='number'> {index+1}</span>
+    </article>
+  )
+}
+
+export default BookList
+
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+root.render(<BookList />)
